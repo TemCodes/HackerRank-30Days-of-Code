3acb974fee011be7f6eb1e8d116ce7969c7824f4
TemCodes <artemgareyss@gmail.com>
2018-10-10 19:48:01 -0700

    Added my first few days!

---
+++ Day1.py
@@ -0,0 +1,20 @@
+
+# Declare second integer, double, and String variables.
+f = 0
+g   = 0.0
+c = ''
+
+# Read and save an integer, double, and String to your variables.
+f = input()
+g = input()
+c = input()
+
+# Print the sum of both integer variables on a new line.
+print(i+int(f))
+
+# Print the sum of the double variables on a new line.
+print(d + float(g))
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + c)
